e7c42202622f034ea1e71479104d9876a9590bec
bye-shuvo <abuhasnatshuvo605@gmail.com>
2025-02-14 23:29:15 +0600

    Update CropDropDown.jsx

--- src/Components/CropDropDown.jsx
+++ src/Components/CropDropDown.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
-import cropsJson from "../assets/Crops.json";
 
 const CropDropDown = ({ handleValue, handleType, handleCategory , selectedValue }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -13,7 +12,7 @@ const CropDropDown = ({ handleValue, handleType, handleCategory , selectedValue 
 
   //Fetching Crops data
   const fetchCrops = async () => {
-    const response = await fetch(cropsJson);
+    const response = await fetch("https://seed-calculator.vercel.app/api/crops");
     const data = await response.json();
     setCrops(data.crops);
   };
